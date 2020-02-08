d9b07de273df5a0b50cbb47dbef18de5fef20c44
David Tran <tran.a.david@gmail.com>
2020-02-08 13:16:35 -0500

    my nes stuff

--- test2.py
+++ test2.py
@@ -11,13 +11,8 @@ def test2():
 
 test1()
 test2()
-<<<<<<< HEAD
+
 #Pull from Master/develop to update local master/develop
 #Go to your branch and rebase with master/develop
 #This should pull the latest and greatest into your branch
 #this is what you should do.
-=======
-#my struff
-test() test test2
-#my new stuff
->>>>>>> my nes stuff
